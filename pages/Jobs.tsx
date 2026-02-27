427257bd58d19cc614c5a869a0e47ed6415ae227
Johan Tijsseling <jtijs@MacBook-Pro-van-Johan.local>
2026-02-27 09:50:08 +0100

    Adjust desktop opacity of subpage wine stains to make them slightly more visible

--- pages/Jobs.tsx
+++ pages/Jobs.tsx
@@ -6,8 +6,17 @@ import { Input, TextArea } from '../components/FormElements';
 export const Jobs: React.FC = () => {
  return (
  <div className="min-h-screen pt-24 md:pt-32 pb-12 px-6 fade-in max-w-3xl mx-auto bg-brand-cream">
-                                                {/* Premium Abstract Wine Stain Texture (Top Right) */}
-            <div className="fixed -right-[30%] md:-right-[15%] -top-[10%] md:-top-[20%] w-[600px] h-[600px] md:w-[1200px] md:h-[1200px] pointer-events-none select-none opacity-10 rotate-45 z-0 bg-contain bg-center bg-no-repeat" style={{ backgroundImage: "url('/wine-stain-25.png')" }} aria-hidden="true" />
+                                                                                    {/* Premium Abstract Wine Stain Texture (Top Right) */}
+            <div className="fixed -right-[30%] md:-right-[15%] top-[10%] md:-top-[20%] w-[450px] h-[450px] md:w-[1200px] md:h-[1200px] pointer-events-none select-none opacity-[0.15] md:opacity-30 rotate-45 z-0 bg-contain bg-center bg-no-repeat" style={{ backgroundImage: "url('/wine-stain-25.png')" }} aria-hidden="true" />
+            
+            {/* Premium Abstract Wine Stain Texture (Bottom Left) */}
+            <div className="fixed -left-[30%] md:-left-[15%] bottom-[0%] md:-bottom-[20%] w-[450px] h-[450px] md:w-[1000px] md:h-[1000px] pointer-events-none select-none opacity-10 md:opacity-20 -rotate-12 scale-x-[-1] z-0 bg-contain bg-center bg-no-repeat" style={{ backgroundImage: "url('/wine-stain-25.png')" }} aria-hidden="true" />
+            
+            {/* Premium Abstract Wine Stain Texture (Bottom Left) */}
+            <div className="fixed -left-[30%] md:-left-[15%] bottom-[0%] md:-bottom-[20%] w-[450px] h-[450px] md:w-[1000px] md:h-[1000px] pointer-events-none select-none opacity-10 md:opacity-[0.15] -rotate-12 scale-x-[-1] z-0 bg-contain bg-center bg-no-repeat" style={{ backgroundImage: "url('/wine-stain-25.png')" }} aria-hidden="true" />
+            
+            {/* Premium Abstract Wine Stain Texture (Bottom Left) */}
+            <div className="fixed -left-[30%] md:-left-[15%] bottom-[0%] md:-bottom-[20%] w-[450px] h-[450px] md:w-[1000px] md:h-[1000px] pointer-events-none select-none opacity-10 md:opacity-5 -rotate-12 scale-x-[-1] z-0 bg-contain bg-center bg-no-repeat" style={{ backgroundImage: "url('/wine-stain-25.png')" }} aria-hidden="true" />
             
             {/* Premium Abstract Wine Stain Texture (Bottom Left) */}
             <div className="fixed -left-[30%] md:-left-[15%] -bottom-[10%] md:-bottom-[20%] w-[500px] h-[500px] md:w-[1000px] md:h-[1000px] pointer-events-none select-none opacity-5 -rotate-12 scale-x-[-1] z-0 bg-contain bg-center bg-no-repeat" style={{ backgroundImage: "url('/wine-stain-25.png')" }} aria-hidden="true" />
